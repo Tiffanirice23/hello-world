e85ae911dccd20a07db542ff2b342d1f8693bd38
Tiffani Rice <TiffaniRice414@gmail.com>
2023-03-23 12:46:45 -0700

    More JS added w CSS edits

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 alert ("Over 50% of commercial pilots have admitted to falling asleep while flying a plane..");
 
-let vacation = prompt("... Are you happy today? (Yes, No, Maybe So)");
-console.log(vacation);
+function greetUser (){
+    let vacation = prompt("... Are you happy today? (Yes, No, Maybe So)");
+    console.log(vacation);
 
-if (vacation == 'Yes'){
-    document.write('Congrats on your brain chemistry!')
-} else if (vacation == 'No'){
-    document.write('Bestie, same!')
-} else if (vacation == 'Maybe So'){
-    document.write('... How mysterious of you')
+    if (vacation == ""){
+        vacation = prompt("Try working on your reading comprehension skills, pal! :)");
+    }
+    else if (vacation == 'Yes'){
+        document.write('Way to not let sleeping airline pilots stop you from staying happy! *side eye*')
+    } else if (vacation == 'No'){
+        document.write('Bestie, same!')
+    } else if (vacation == 'Maybe So'){
+        document.write('... How mysterious of you')
+    } else {
+        document.write('That was not an option! You Failed the Test')
+    }
+    
+    return vacation;
 }
+
+let theirAnswer = greetUser ();
+console.log (theirAnswer);
 
 console.log("After Conditional");
 
