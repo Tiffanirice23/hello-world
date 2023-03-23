0e1646e485c63901ea42275b921c835b7086df9d
Tiffani Rice <TiffaniRice414@gmail.com>
2023-03-22 20:47:52 -0700

    JS code and Style

---
+++ script.js
@@ -0,0 +1,17 @@
+// alert ("Over 50% of commercial pilots have admitted to falling asleep while flying a plane..");
+
+// let vacation = prompt("... Are you happy today? (Yes, No, Maybe So)");
+// console.log(vacation);
+
+// if (vacation == 'Yes'){
+//     document.write('Congrats on your brain chemistry!')
+// } else if (vacation == 'No'){
+//     document.write('Bestie, same!')
+// } else if (vacation == 'Maybe So'){
+//     document.write('... How mysterious of you')
+// }
+
+// console.log("After Conditional");
+
+
+// confirm ("Are you SURE you want to see this mess of a Website?!!")
