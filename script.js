15b5a440507ebf81f8f3e5001c5b3eba6e7b2d8f
Tiffani Rice <TiffaniRice414@gmail.com>
2023-03-23 21:31:28 -0700

    styling edits

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function greetUser() {
-    let vacation = prompt("... Are you happy today? (Yes, No, Maybe So)");
-    console.log(vacation);
+     let vacation = prompt("... Are you happy today? (Yes, No, Maybe So)");
+     console.log(vacation);
     
     while (vacation !== "Yes" && vacation !=="No" && vacation !== "Maybe So"){
-        vacation = prompt ("Lol... What :)! Valid answers are Yes, No, or Maybe So");
-    }
+    vacation = prompt ("... bruh. Valid answers are Yes, No, or Maybe So");
+   }
     
     if (vacation == 'Yes'){
-        document.write('Congrats on your brain chemistry!')
+        document.write('Love that for you. Congrats on your brain chemistry!')
     } else if (vacation == 'No'){
-        document.write('Saaame!')
+        document.write('It is 9:47 pm as I type this lol. Samsiesss')
     } else if (vacation == 'Maybe So'){
-        document.write('... How mysterious of you')
+        document.write('... How mysterious of you.')
     } else {
         vacation = prompt("Try working on your reading comprehension skills, pal! :)");
         document.write('wamp waaamp')
@@ -19,7 +19,6 @@ function greetUser() {
     
     return vacation;
 }
-
 
 alert ("Over 50% of commercial pilots have admitted to falling asleep while flying a plane..");
 
@@ -29,4 +28,4 @@ console.log (theirAnswer);
 console.log("After Conditional");
 
 
-confirm ("Are you SURE you want to see this mess of a Website?!!")
+confirm ("Are you SURE you want to see the mess in here? Last chance to turn around!")
