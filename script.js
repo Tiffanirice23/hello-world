f1ed3249864fb6a0adbade0c801b5a79d08fcb3f
Tiffani Rice <TiffaniRice414@gmail.com>
2023-03-24 21:10:29 -0700

    Functionality changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function greetUser(){
      console.log(vacation);
     
      while (vacation !== "yes" && vacation !=="no" && vacation !== "maybe so"){
-     vacation = prompt ("... Bruh. Valid answers are Yes, No, or Maybe So");
+     vacation = prompt ("... Bruh. Valid answers are Yes, No, or Maybe So").toLowerCase();
     }
      
      if (vacation == 'yes'){
@@ -25,7 +25,7 @@ alert ("Taking a vacation (even a stay-cation!) can lower the risk of heart dise
 
 
 function mentalHealth(){
-    let displayBeach = prompt("Scale of 1-5, how much do you need a vacation rn?");
+    let displayBeach = prompt("How many flights outta here this year do you want? (Sending up to 5 planes to your location, beep boop)");
         displayBeach = parseInt(displayBeach);
         console.log(displayBeach);
     while (displayBeach < 1 || displayBeach > 5){
